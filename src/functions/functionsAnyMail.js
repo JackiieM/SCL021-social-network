fe709cf95d0d13d77575c5d32f108b397b18b771
Consuelo Gomez <mconsuelo.gomezt@gmail.com>
2022-08-19 12:15:26 -0400

    actualizar archivos

--- src/functions/functionsAnyMail.js
+++ src/functions/functionsAnyMail.js
@@ -1,18 +1,2 @@
-//registrarse con cualquier correo
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-
-    //crear usuario con correo electronico
-    const auth = getAuth();
-    createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // Signed in
-            const user = userCredential.user;
-            // ...
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // ..
-        });
-
+export default () =>
+console.log("estás en anymail")
--- src/functions/functionsLogIn.js
+++ src/functions/functionsLogIn.js
@@ -1,25 +1,2 @@
-//login
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-
-const auth = getAuth();
-signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
-
-//logout 
-// import { getAuth, signOut } from "firebase/auth";
-
-// const auth = getAuth();
-// signOut(auth).then(() => {
-//   // Sign-out successful.
-// }).catch((error) => {
-//   // An error happened.
-// });
+export default () =>
+console.log("estás en login")
